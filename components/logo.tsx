d57d27575642122d70c1cf60e070f3ac134b5011
Tim Neutkens <timneutkens@me.com>
2020-07-03 19:35:35 +0200

    Test out new mode

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,24 +1,26 @@
-export default (props: any) => {
-  return <svg
-  width="50px"
-  height="55px"
-  viewBox="0 0 50 55"
-  version="1.1"
-  xmlnsXlink="http://www.w3.org/1999/xlink"
-  {...props}
->
-  <g
-    transform="translate(-325.000000, -123.000000) translate(207.000000, 123.000000) translate(117.957447, 0.000000)"
-    fill="#000"
-    fillRule="nonzero"
-    stroke="none"
-    strokeWidth={1}
-  >
-    <polygon points="14.7446809 0 49.1489362 0 49.1489362 14.7446809 0 14.7446809" />
-    <polygon
-      transform="translate(24.574468, 35.223404) scale(-1, 1) rotate(-270.000000) translate(-24.574468, -35.223404) "
-      points="19.6595745 27.8510638 44.2340426 27.8510638 44.2340426 42.5957447 4.91489362 42.5957447"
-    />
-  </g>
-</svg>
+export default function Logo(props: any) {
+  return (
+    <svg
+      width="50px"
+      height="55px"
+      viewBox="0 0 50 55"
+      version="1.1"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      {...props}
+    >
+      <g
+        transform="translate(-325.000000, -123.000000) translate(207.000000, 123.000000) translate(117.957447, 0.000000)"
+        fill="#000"
+        fillRule="nonzero"
+        stroke="none"
+        strokeWidth={1}
+      >
+        <polygon points="14.7446809 0 49.1489362 0 49.1489362 14.7446809 0 14.7446809" />
+        <polygon
+          transform="translate(24.574468, 35.223404) scale(-1, 1) rotate(-270.000000) translate(-24.574468, -35.223404) "
+          points="19.6595745 27.8510638 44.2340426 27.8510638 44.2340426 42.5957447 4.91489362 42.5957447"
+        />
+      </g>
+    </svg>
+  );
 }
