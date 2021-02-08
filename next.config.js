8ffffce162ba4ab956b4f3c24aa3c933b6761d7f
Tim Neutkens <timneutkens@me.com>
2021-02-08 13:18:53 +0100

    Use webpack 5 + stats

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 // Enables Next.js i18n routing
 module.exports = {
+  experimental: {
+    stats: true,
+  },
+  future: {
+    webpack5: true,
+  },
   i18n: {
     locales: ["nl", "en"],
     defaultLocale: "en",
