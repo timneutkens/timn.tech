4a72145f7817a4d0c38105dd49caa3afb8761d0e
Tim Neutkens <timneutkens@me.com>
2021-08-31 11:24:18 +0200

    Disable experiments

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  experimental: {
-    swcLoader: true,
-    swcMinify: true,
-  },
+  // experimental: {
+  //   swcLoader: true,
+  //   swcMinify: true,
+  // },
   i18n: {
     locales: ["nl", "en"],
     defaultLocale: "en",
