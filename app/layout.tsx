0a54ca91a011445529eeb30ea3eedfc5a9ba121c
Tim Neutkens <tim@timneutkens.nl>
2023-05-15 17:43:23 +0200

    Upgrade Next.js

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,14 @@
 import "../styles/global.css";
 import Analytics from "../components/analytics";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Tim Neutkens",
+  viewport: "width=device-width, initial-scale=1",
+  description:
+    "Hi! I'm Tim Neutkens, a software engineer from The Netherlands.",
+};
+
 export default function RootLayout({
   children,
 }: {
